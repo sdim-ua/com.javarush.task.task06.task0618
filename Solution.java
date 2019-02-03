2647747afff7b52e8bb4ab3b626e3e51f4512931
sdim-ua <ingersol@yandex.ua>
2019-02-03 22:05:54 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+/* 
+KissMyShinyMetalAss
+*/
+
+public class Solution {
+    public static class KissMyShinyMetalAss  {
+
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        KissMyShinyMetalAss ass = new KissMyShinyMetalAss();
+        System.out.println(ass);
+    }
+}
